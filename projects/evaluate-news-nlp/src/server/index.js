40ae6618fde534d2a9e15a96e91a2d3118887404
Mohamed Sayed <ms10596@gmail.com>
2021-03-14 20:37:07 +0200

    8081

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 })
 
 app.listen(8081, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
